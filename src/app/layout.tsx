16c28d40ffac0f4f14758b6e8d16953709930b7b
Priya's VivoBook <priyankarkoley@gmail.com>
2025-04-03 00:40:12 +0530

    feat: update theme color in manifest and adjust metadata for improved PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,12 @@ export const metadata: Metadata = {
 	generator: 'Next.js',
 	manifest: '/manifest.json',
 	keywords: ['nextjs', 'next14', 'pwa', 'next-pwa'],
-	themeColor: [{ media: '(prefers-color-scheme: dark)', color: '#fff' }],
 	authors: [
 		{
 			name: 'priyankarkoley',
 			url: 'https://priyankarkoley.com',
 		},
 	],
-	viewport: 'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
 };
 
 export default function RootLayout({
